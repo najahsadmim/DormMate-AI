2c4d5fb9e45a442fe3cd7f26a6f0f13dd004ab24
Najah Sadmim <136795896+najahsadmim@users.noreply.github.com>
2026-07-08 02:30:08 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
