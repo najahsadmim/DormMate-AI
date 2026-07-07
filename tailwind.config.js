86ef793f44c78bc78f4cd298ef278234d96f2b9c
Najah Sadmim <136795896+najahsadmim@users.noreply.github.com>
2026-07-08 02:31:18 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tangerine: '#F28C28', 
+        forestGreen: '#228B22',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
