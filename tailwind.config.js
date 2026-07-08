f48e143d7298b5ce1627e61d4536e5c2b74a5c73
snadwich <79296594+snadwich01@users.noreply.github.com>
2026-07-09 01:59:04 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
+        // These names (tangerine, forestGreen) allow you to use 
+        // classes like 'text-tangerine' or 'bg-forestGreen'
         tangerine: '#F28C28', 
         forestGreen: '#228B22',
       },
       fontFamily: {
+        // This allows you to use 'font-poppins' in your components
         poppins: ['Poppins', 'sans-serif'],
       },
     },
