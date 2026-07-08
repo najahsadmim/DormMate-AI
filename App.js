6a0c0642add9a79f5b856761774ceed6c9a4424e
snadwich <79296594+snadwich01@users.noreply.github.com>
2026-07-09 01:22:53 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
 import Navbar from './components/Navbar';
-import HomePage from './components/HomePage';
+
+// Pages
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import PantryPage from './pages/PantryPage';
+import SavedPage from './pages/SavedPage';
+import ProfilePage from './pages/ProfilePage';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App bg-white min-h-screen">
+        {/* Persistent Navbar across all routes */}
+        <Navbar />
+        
+        {/* Dynamic Page Content */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/pantry" element={<PantryPage />} />
+          <Route path="/saved" element={<SavedPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
